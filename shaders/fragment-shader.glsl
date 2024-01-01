5c2ceceab7e6dcc55d4ac5c4afb590b1c2e58900
GonnaFlyMethod <alex.husakou@gmail.com>
2024-01-01 20:26:36 +0100

    shader/color-output-shader

---
+++ shaders/fragment-shader.glsl
@@ -0,0 +1,21 @@
+
+
+void main() {
+  // Black
+  // gl_FragColor = vec4(0.0);
+
+  // White
+  // gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+  // Green
+  // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+
+  // Yellow
+  // gl_FragColor = vec4(1.0, 1.0, 0.0,  1.0);
+
+  // Blue
+  // gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+
+  // Custom
+  gl_FragColor = vec4(125.0 / 255.0, 0.0, 0.0, 0.0);
+}
